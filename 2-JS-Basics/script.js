aefca48a04d81f6181654cc99c52be5b27c4d489
YudhaYuki <YudhaYuki@hotmail.com>
2018-04-07 22:23:45 +0200

    If else statement

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,6 +1,58 @@
+/*
 let name = "John";
 let lastName = "Smith";
 let age = 26;
 let fullAge = true;
 
 console.log(`${name} ${lastName} is ${age} years old. ${fullAge}`);
+*/
+
+
+
+
+
+
+
+
+
+///////////////////////// OPEATORS /////////////////////////////////
+
+/*
+var now = 2016;
+var birthYear = now - 26 ;
+
+birthYear = now - 26 * 2;
+
+console.log(birthYear);
+*/
+
+
+
+
+///////////////////////// IF ELSE STATEMENT /////////////////////////////////
+
+var name = "John";
+var age = "26";
+var isMarried = true;
+
+/*
+if (isMarried === true ) {
+    console.log(`${name} is ${age} years old and he is married`);
+} else {
+    console.log(`${name} is ${age} years old and he is single`);    
+}
+*/
+
+
+/*
+if (isMarried) {
+    console.log(`${name} is ${age} years old and he is married`);
+} 
+*/
+
+isMarried ? console.log(`${name} is ${age} years old and he is married`) : console.log(`${name} is ${age} years old and he is single`);    
+
+
+var num = 23 + "23";
+
+console.log(num);
